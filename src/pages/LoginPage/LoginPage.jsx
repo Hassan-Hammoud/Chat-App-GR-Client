8c7d4a9519dd5825186a2e11c4712e77a25b32de
Hassan Hammoud <hassanhammoud2000@gmail.com>
2025-10-06 21:23:14 +0300

    Implement LoginPage and ProfilePage components

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,11 +1,121 @@
 /** @format */
+import { useState } from 'react';
+import assets from './../../assets/assets';
 
 const LoginPage = () => {
+  const [currState, SetCurrState] = useState('Sign up');
+  const [fullName, SetFullName] = useState('');
+  const [email, SetEmail] = useState('');
+  const [password, SetPassword] = useState('');
+  const [bio, SetBio] = useState('');
+  const [isDataSubmitted, SetIsDataSubmitted] = useState(false);
+
   return (
-    <div className=''>
-      <h1>Login Page</h1>
+    <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
+      {/* ******** LEFT SIDE ******** */}
+      <img
+        className='w-[min[30vw, 250px]]'
+        src={assets.logo_big}
+        alt='Big logo image'
+      />
+      {/* ******** RIGHT SIDE ******** */}
+      <form className='border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg'>
+        <h2 className='font-medium text-2xl flex justify-between items-center'>
+          {currState}
+          <img
+            className='w-5 cursor-pointer'
+            src={assets.arrow_icon}
+            alt='Arrow Icon '
+          />
+        </h2>
+        {currState === 'Sign up' && !isDataSubmitted && (
+          <input
+            onChange={(e) => SetFullName(e.target.value)}
+            value={fullName}
+            type='text'
+            className='p-2 border border-gray-500 rounded-md focus:outline-none'
+            placeholder='Full Name'
+            required
+          />
+        )}
+
+        {!isDataSubmitted && (
+          <>
+            <input
+              onChange={(e) => SetEmail(e.target.value)}
+              value={email}
+              type='email'
+              placeholder='Email Address'
+              required
+              className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500'
+            />
+            <input
+              onChange={(e) => SetPassword(e.target.value)}
+              value={password}
+              type='password'
+              placeholder='Password'
+              required
+              className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500'
+            />
+          </>
+        )}
+
+        {currState === 'Sign up' && isDataSubmitted && (
+          <textarea
+            onChange={(e) => SetBio(e.target.value)}
+            value={bio}
+            rows={4}
+            className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500'
+            placeholder='Provide a short bio...'
+            required
+          ></textarea>
+        )}
+
+        <button
+          type='submit'
+          className='py-3 bg-gradient-to-r from-purple-400 to-violet-600 text-white rounded-md cursor-pointer'
+        >
+          {currState === 'Sign up' ? 'Create Account' : 'Login Now'}
+        </button>
+        <div className='flex items-center gap-2 text-sm text-gray-500'>
+          <input
+            type='checkbox'
+            className=''
+          />
+          <p className=''>Agree to the terms of use & privacy policy</p>
+        </div>
+        <div className='flex flex-col gap-2'>
+          {currState === 'Sign up' ? (
+            <p className='text-sm text-gray-600'>
+              Already have an account{' '}
+              <span
+                onClick={() => {
+                  SetIsDataSubmitted(false);
+                  SetCurrState('Login');
+                }}
+                className='font-medium text-violet-500 cursor-pointer'
+              >
+                Login here
+              </span>
+            </p>
+          ) : (
+            <p className='text-sm text-gray-600'>
+              Create an account{' '}
+              <span
+                onClick={() => {
+                  SetCurrState('Sign up');
+                }}
+                className='font-medium text-violet-500 cursor-pointer'
+              >
+                Click here
+              </span>
+            </p>
+          )}
+        </div>
+      </form>
     </div>
   );
 };
 
 export default LoginPage;
+// ! One hour and 55 minutes left
